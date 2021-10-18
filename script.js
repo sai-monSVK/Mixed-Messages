1d2e9bf61ff7a99026d37f134c7730fdc957d8d8
sai-monSVK <91848785+sai-monSVK@users.noreply.github.com>
2021-10-18 15:35:26 +0200

    started creating variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Fields with messages parts. 
+
+const who = ['I always', "You can't ", 'Life is ', 'Work until ', 'If you ', 'Bad decisions'];
+const what = ['say that ', 'wanted ', ''];
+const conclusion = [];
+
