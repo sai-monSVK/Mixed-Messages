dd14bc0857149e707744d4f9f60868c43a8f5b24
sai-monSVK <91848785+sai-monSVK@users.noreply.github.com>
2021-10-19 13:59:22 +0200

    new for with object instead of multiple arrays

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Fields with messages parts. 
+//Object with message parts
 
-const firstPart = ['Life ', 'World ', 'Universe ', 'Luck ', 'Your yourney, ', 'Path ', 'Destiny ', 'Love ', 'Imagination ' ];
-const secondPart = ['is always made of ', 'truly is full of ', 'can be filled with', 'is like ', "is never bad thing, it's like ", 'is like a river always reflect '];
-const thirdPart = ['beautifull things.', 'shiny rainbows.', 'crazy unicorns.', 'hearthwarming dragons.', 'pandas.', 'bank empolyee.', 'gates of hell.'];
+const messageParts = {
+
+firstPart : ['Life ', 'World ', 'Universe ', 'Luck ', 'Your yourney, ', 'Path ', 'Destiny ', 'Love ', 'Imagination ' ],
+secondPart : ['is always made of ', 'truly is full of ', 'can be filled with', 'is like ', "is never bad thing, it's like ", 'is like a river always reflect '],
+thirdPart : ['beautifull things.', 'shiny rainbows.', 'crazy unicorns.', 'hearthwarming dragons.', 'pandas.', 'bank empolyee.', 'gates of hell.'],
+};
 
 //function for pick random string from array
 
-function randNum(arr) {
-        let n = Math.floor(Math.random()*arr.length);
+function randNum() {
+        let n = Math.floor(Math.random());
         return n;
     };
 
