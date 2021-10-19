c93ae1fb42ba41f64d1a4cd796cf66c0ffc5f46f
sai-monSVK <91848785+sai-monSVK@users.noreply.github.com>
2021-10-19 12:51:43 +0200

    first version

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 // Fields with messages parts. 
 
-const who = ['I always', "You can't ", 'Life is ', 'Work until ', 'If you ', 'Bad decisions'];
-const what = ['say that ', 'wanted ', ''];
-const conclusion = [];
+const firstPart = ['Life ', 'World ', 'Universe ', 'Luck ', 'Your yourney, ', 'Path ', 'Destiny ', 'Love ', 'Imagination ' ];
+const secondPart = ['is always made of ', 'truly is full of ', 'can be filled with', 'is like ', "is never bad thing, it's like ", 'is like a river always reflect '];
+const thirdPart = ['beautifull things.', 'shiny rainbows.', 'crazy unicorns.', 'hearthwarming dragons.', 'pandas.', 'bank empolyee.', 'gates of hell.'];
+
+//function for pick random string from array
+
+function randNum(arr) {
+        let n = Math.floor(Math.random()*arr.length);
+        return n;
+    };
+
+
+//create variables for every part
+
+let a = randNum(firstPart);
+let b = randNum(secondPart);
+let c = randNum(thirdPart);
+
+
+//func for mixed messages
+function mixedMessages() {
+    let message = firstPart[a] + secondPart[b] + thirdPart[c];    
+    return message;
+}
+
+console.log(mixedMessages());
 
